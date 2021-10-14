55a7529980fe3dcb08d26ba95b6df7c83a90581f
Jenish-gif <62330210+Jenish-gif@users.noreply.github.com>
2021-10-14 19:36:56 +0930

    Adding new screen to the app

--- components/quiz.js
+++ components/quiz.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -15,7 +15,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container:{
-    paddingTop: 90,
-    paddingHorizontal: 16,
+    
   }
 });
