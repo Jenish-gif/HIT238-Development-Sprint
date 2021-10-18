f28725dd52a636e662c6810d56266d2a46f645b2
Jenish-gif <62330210+Jenish-gif@users.noreply.github.com>
2021-10-18 16:37:57 +0930

    Quiz page designed with api generated questions

--- components/quiz.js
+++ components/quiz.js
@@ -1,74 +1,129 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
-export default function Quiz() {
+const Quiz = ({navigation}) => {
+    const [questions, setQuestions] = useState();
+    const getQuiz= async() => {
+        const url='https://opentdb.com/api.php?amount=12&category=18&difficulty=medium&type=multiple';
+        const res = await fetch(url);
+        console.log(res);
+        const data = await res.json();
+        console.log(data.results[0]);
+        setQuestions(data.results);
+    };
+
+    useEffect(() => {
+        getQuiz();
+
+    }, []);
+
   return (
-    <NavigationContainer>
-    <View style={styles.container}>
+       <View style={styles.container}>
 
       <View style={styles.question}>
-          <Text>
+          <Text style={styles.questionName}> 
               Q1. What is HTML?
           </Text>
       </View>
 
       <View style={styles.answer}>
-        <TouchableOpacity>       
-            <Text>Desktop Application</Text>
+        <TouchableOpacity style={styles.answerBox}>       
+            <Text style={styles.answerName}>Desktop Application</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>       
-            <Text>Programming Language</Text>
+        <TouchableOpacity style={styles.answerBox}>       
+            <Text style={styles.answerName} >Programming Language</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>       
-            <Text>Mobile Application</Text>
+        <TouchableOpacity style={styles.answerBox}>       
+            <Text style={styles.answerName}>Mobile Application</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>       
-            <Text>Mark's Language</Text>
+        <TouchableOpacity style={styles.answerBox} >       
+            <Text style={styles.answerName}>Mark's Language</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.button}>
-      <TouchableOpacity>       
-            <Text>SKIP</Text>
+      <View style={styles.Bottom}>
+      <TouchableOpacity style={styles.button}>       
+            <Text style={styles.buttonText}>SKIP</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>       
-            <Text>NEXT</Text>
+        <TouchableOpacity style={styles.button}>       
+            <Text  style={styles.buttonText}>NEXT</Text>
         </TouchableOpacity>
 
       </View>
 
     </View>
-    </NavigationContainer>
+   
   );
 }
 
+export default Quiz
+
 const styles = StyleSheet.create({
 
-  container:{
+    container:{
       padding: 15,
       height: '100%', 
-  },
+    },
 
-  question:{
-      marginVertical: 20,
-  },
+    question:{
+        marginVertical: 20,
+        fontSize: 25,
+        textAlign: "left",
+        backgroundColor: "#95d5b2",
+        padding: 20,
+        borderColor: "white",
+        borderWidth: 2,
+    },
 
-  answer:{
+    answer:{
       marginVertical: 15,
       flex: 1,
-  },
+      paddingLeft: 10,
+    },
 
-  button:{
-      marginBottom: 15,
-      paddingVertical: 15,
-      justifyContent:"space-between",
-      flexDirection: "row",
-  },
+    Bottom:{    
+        fontSize: 20, 
+        justifyContent:"space-between",
+        flexDirection: "row",
+        paddingBottom: 150,
+    },
+
+    button:{
+        backgroundColor: "#427aa1",
+        padding: 15,
+        borderRadius: 15,
+        alignItems: "center",
+    },
+
+    buttonText:{    
+        fontSize: 20,  
+        fontWeight: '600',
+        color: 'white',
+    },
+
+    questionName:{
+        fontSize: 20,
+
+    } ,
+
+    answerBox:{
+        borderRadius:15,
+        backgroundColor: '#2d6a4f',
+        marginVertical: 10,
+        padding: 15,
+    },
+
+    answerName:{
+        fontSize: 20,
+        color: 'white',
+        fontWeight: '500',
+    },
 
 });
